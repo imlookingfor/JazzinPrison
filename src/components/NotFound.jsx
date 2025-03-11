6083887e5a87e08f2487d0b145264ed53ea63764
gnosneebah <imlookingformyjob@gmail.com>
2025-03-11 14:41:15 +0900

    modify

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,8 +1,11 @@
 function NotFound() {
   return (
-    <div>
-      Not Found bro 접속 again
-    </div>
+    <>
+      <meta http-equiv="refresh" content="0; url=./" />
+      <div>
+        Not Found bro 접속 again
+      </div>
+    </>
   )
 }
 
