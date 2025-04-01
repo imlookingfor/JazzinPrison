a5e71f0558dc61f0c0ecd6091e1117a2e36bab3c
gnosneebah <imlookingformyjob@gmail.com>
2025-04-01 16:09:05 +0900

    change profile

--- src/components/Directors.jsx
+++ src/components/Directors.jsx
@@ -1,7 +1,7 @@
 export default function Directors(props) {
   return(
     <li>
-      <img src={props.image} alt="" />     
+      {/* <img src={props.image} alt="" /> */}
       <h3>{props.name}</h3>
       <h4>{props.role}</h4>
     </li>
