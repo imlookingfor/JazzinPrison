a5a9e53a729d817bf7bb30baf113104af8cf1cad
gnosneebah <imlookingformyjob@gmail.com>
2025-04-01 21:28:02 +0900

    modify

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -13,6 +13,7 @@ function Home() {
         {/* <img className="home-logo" src={logoImg} alt="" /> */}
         <h1 className='home-h1'>두 여자 이야기</h1>
       </div>
+      <div className='hidden-div'></div>
       <div className='home-btn-group'>
         {/* {<button onClick={() => window.open("#")}>예매하기</button>} */}
         <Link to='/info'><button className='home-btn'>공연소개</button></Link>
